2df739e6089120f551cd9db39f4ba2c47f8f520b
stormynight9 <ferjani.nader@hotmail.fr>
2022-03-25 03:54:13 +0100

    added navbar and hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+
+
+const Hero = () => {
+    return (
+        <section className="mt-24 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8 ">
+            <div className="text-center space-y-4">
+                <h1 className="text-gray-800 font-bold text-4xl md:text-5xl">
+                    Trouver les experiences que vous
+                    <span className="text-rose-600"> voulez vivre</span>
+                </h1>
+                <p className="text-gray-500 max-w-xl mx-auto leading-relaxed">
+                    Hobbizer organise et crée des activités uniques pour tous vos évènements à Paris. Hobbizer organise et crée des activités uniques pour
+                </p>
+            </div>
+            <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
+                <a href="javascript:void(0)" className="px-10 py-3.5 w-full bg-rose-600 text-white text-center rounded-md shadow-md block sm:w-auto">
+                    Get started
+                </a>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useRef, useState } from 'react'
+
+const Navbar = () => {
+
+    const [state, setState] = useState(false)
+    const navRef = useRef()
+
+    // Replace javascript:void(0) path with your path
+    const navigation = [
+        { title: "Customers", path: "javascript:void(0)" },
+        { title: "Careers", path: "javascript:void(0)" },
+        { title: "Guides", path: "javascript:void(0)" },
+        { title: "Partners", path: "javascript:void(0)" },
+        { title: "Teams", path: "javascript:void(0)" },
+        { title: "Blog", path: "javascript:void(0)" }
+    ]
+
+    useEffect(() => {
+
+        const body = document.body
+
+        // Disable scrolling
+        const customBodyStyle = ["overflow-hidden", "md:overflow-visible"]
+        if (state) body.classList.add(...customBodyStyle)
+        // Enable scrolling
+        else body.classList.remove(...customBodyStyle)
+
+        // Sticky strick
+        const customStyle = ["sticky-nav", "fixed", "border-b"]
+        window.onscroll = () => {
+            if (window.scrollY > 80) navRef.current.classList.add(...customStyle)
+            else navRef.current.classList.remove(...customStyle)
+        }
+    }, [state])
+
+
+    return (
+        <nav ref={navRef} className="bg-white w-full top-0 z-20 border-b">
+            <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
+                <div className="flex items-center justify-between py-3 md:py-4 md:block">
+                    <a href="javascript:void(0)">
+                        <img
+                            src="https://hobbizer.com/bundles/hobbizerorganiser/images/logo.png"
+                            width={120}
+                            height={50}
+                            alt="Float UI logo"
+                        />
+                    </a>
+                    <div className="md:hidden">
+                        <button className="text-rose-700 outline-none p-2 rounded-md focus:border-rose-400 focus:border"
+                            onClick={() => setState(!state)}
+                        >
+                            {
+                                state ? (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                                    </svg>
+                                ) : (
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                                    </svg>
+                                )
+                            }
+                        </button>
+                    </div>
+                </div>
+                <div className={`flex-1 justify-between flex-row-reverse md:overflow-visible md:flex md:pb-0 md:pr-0 md:h-auto ${state ? 'h-screen pb-20 overflow-auto pr-4' : 'hidden'}`}>
+                    <div>
+                        <ul className="flex flex-col-reverse space-x-0 md:space-x-6 md:flex-row">
+                            <li className="mt-4 md:mt-0">
+                                <a href="javascript:void(0)" className="py-3 px-4 text-center border  text-gray-600 hover:text-rose-600 rounded-md block md:inline md:border-0">
+                                    How it works
+                                </a>
+                            </li>
+                            <li className="mt-4 md:mt-0">
+                                <a href="javascript:void(0)" className="py-3 px-4 text-center border text-gray-600 hover:text-rose-600 rounded-md block md:inline ">
+                                    Login
+                                </a>
+                            </li>
+                            <li className="mt-8 md:mt-0">
+                                <a href="javascript:void(0)" className="py-3 px-4 text-center text-white bg-rose-600 hover:bg-rose-700 rounded-md shadow block md:inline">
+                                    Sign Up
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
