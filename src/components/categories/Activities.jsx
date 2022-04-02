89ad007fbd924f1100f0753dab6569940ce970c0
stormynight9 <ferjani.nader@hotmail.fr>
2022-04-02 07:41:18 +0100

    added dynamic routing for activities

--- src/components/categories/Activities.jsx
+++ src/components/categories/Activities.jsx
@@ -1,4 +1,4 @@
-import { useParams, NavLink } from 'react-router-dom'
+import { useParams, NavLink, Link } from 'react-router-dom'
 import { activities } from '../../constants/activities'
 import { categories } from '../../constants/categories'
 import Timeline from '../shared/Timeline'
@@ -21,11 +21,11 @@ const Activities = () => {
                     <div className='p-4'>
                         <div className='flex gap-2 flex-wrap justify-center md:justify-start'>
                             {activitiesByCategory.map(activity => (
-                                <a key={activity.id} href="javascript:void(0)" className='relative max-w-xs group flex flex-col border-[1px] rounded-md shadow-sm '>
+                                <Link to={`/activities/${activity.id}`} key={activity.id} href="javascript:void(0)" className='relative max-w-xs group flex flex-col border-[1px] rounded-md shadow-sm '>
                                     <img className='object-cover h-44 rounded-t-md group-hover:brightness-75 duration-200' src={activity.coverImage} alt="" />
                                     <span className='absolute rounded-tl-md rounded-br-md top-0 left-0 bg-[#000000AA] group-hover:bg-hobbizer duration-200 p-2 text-white text-3xl font-bold'>{activity.price}$<span className='text-base'>/pers</span></span>
                                     <span className='p-1 font-medium text-center my-auto'>{activity.title}</span>
-                                </a>
+                                </Link>
                             ))}
                         </div>
                     </div>
