467900363dfcdfd3931f506686586aa81a3db06e
stormynight9 <ferjani.nader@hotmail.fr>
2022-04-03 06:49:32 +0100

    redirect on invalid activity link

--- src/components/Activity/Activity.jsx
+++ src/components/Activity/Activity.jsx
@@ -1,10 +1,15 @@
 import SwiperComponent from "./SwiperComponent"
-import { useParams } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
 import { activities } from "../../constants/activities"
 
 const Activity = () => {
     const { activityId } = useParams()
     const activity = activities.find(activity => activity.id === +activityId)
+
+    if (!activity) {
+
+        return <Navigate to="/categories" />
+    }
 
 
 
