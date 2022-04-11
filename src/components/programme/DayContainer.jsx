69a481743f6fd22162189ddbafa488f123d58db0
stormynight9 <ferjani.nader@hotmail.fr>
2022-04-11 08:46:58 +0100

    added responsiveness to programme table

--- src/components/programme/DayContainer.jsx
+++ src/components/programme/DayContainer.jsx
@@ -8,11 +8,11 @@ const dayContainer = (props) => {
 
 
     return (
-        <div className='flex-col w-full sm:max-w-fit pb-4 px-1npm sm:p-4'>
-            <div className='p-4 bg-[#eaeceb] rounded-t-lg'>
+        <div className='flex-col w-full sm:max-w-fit '>
+            <div className=' flex justify-center  rounded-t-lg'>
                 {props.day}
             </div>
-            <div className='flex-col space-y-2 p-4 border-x-[1px] border-b-[1px] rounded-b-lg'>
+            <div className='flex-col space-y-[8px] p-1  rounded-b-lg'>
                 {times.map(time => <ActivityHolder key={time} day={props.day} time={time} />)}
             </div>
 
--- src/components/programme/ProgrammeContainer.jsx
+++ src/components/programme/ProgrammeContainer.jsx
@@ -1,8 +1,9 @@
 import DayContainer from "./DayContainer"
 import dateContext from '../../context/date-context'
-import { useContext, useState } from "react"
+import { useContext, useState, useLayoutEffect, useEffect } from "react"
 import { format } from 'date-fns'
 import ReactPaginate from "react-paginate"
+import './ProgrammeContainer.css'
 
 const Programme = () => {
     const dateCtx = useContext(dateContext)
@@ -11,29 +12,49 @@ const Programme = () => {
 
     const [days, setDays] = useState(dateCtx.datesInterval.slice(0, dateCtx.datesInterval.length))
     const [pageNumber, setPageNumber] = useState(0)
-
-    const daysPerPage = 4
+    const [daysPerPage, setDaysperPage] = useState(4)
     const pagesVisited = pageNumber * daysPerPage
-
     const diplayDays = days.slice(pagesVisited, pagesVisited + daysPerPage).map(day => {
         return <DayContainer key={day} day={day} />
     })
-
     const pageCount = Math.ceil(days.length / daysPerPage)
+
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    useLayoutEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    })
+
+    useEffect(() => {
+        if (screenWidth > 1170) {
+            setDaysperPage(4)
+        } else if (screenWidth > 767) {
+            setDaysperPage(3)
+        } else {
+            setDaysperPage(2)
+        }
+    }, [screenWidth])
+
+
+    console.log(screenWidth)
 
 
     return (
         <div className='mt-36 flex flex-col justify-center items-center'>
             <h2 className='text-2xl font-medium text-gray-700 mb-2 text-center'>Votre programme du <span className='text-hobbizer'>{startDate}</span> au <span className='text-hobbizer'>{endDate}</span></h2>
             <p className='text-gray-500 mb-3 text-center'>Cliquez sur une période de la journée et sélectionnez vos activités</p>
+            <ReactPaginate
+                previousLabel={"Previous"}
+                nextLabel={"Next"}
+                pageCount={pageCount}
+                onPageChange={({ selected }) => setPageNumber(selected)}
+                containerClassName={"pagination"}
+            />
             <div className='flex'>
                 {diplayDays}
-                <ReactPaginate
-                    previousLabel={"Previous"}
-                    nextLabel={"Next"}
-                    pageCount={pageCount}
-                    onPageChange={({ selected }) => setPageNumber(selected)}
-                />
             </div>
         </div>
     )
