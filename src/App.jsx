656d0e9c1133275e81d901aa152c9c9e43dc9a9a
stormynight9 <ferjani.nader@hotmail.fr>
2022-06-20 18:59:36 +0100

    removed the admin stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import { Slide, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,10 +6,8 @@ import Footer from './components/shared/Footer';
 import Navbar from './components/shared/Navbar';
 import ScrollToTop from './components/shared/ScrollToTop';
 import DataContext from './context/data-context';
-import UserContext from './context/user-context';
 import ActivitiesPage from './pages/ActivitiesPage';
 import ActivityPage from './pages/ActivityPage';
-import AdminPage from './pages/AdminPage';
 import CategoriesPage from './pages/CategoriesPage';
 import CheckoutPage from './pages/CheckoutPage';
 import Home from './pages/Home';
@@ -38,7 +36,6 @@ function App() {
               <Route path='/checkout' element={<CheckoutPage />} />
               <Route path='/add-activity' element={<ProviderPage />} />
               <Route path='/booked-activity-list' element={<ProviderBookedActivityListPage />} />
-              <Route path='/admin' element={<AdminPage />} />
               <Route path='/*' element={<Navigate to='/' />} />
             </Routes>
             <ToastContainer position="bottom-center"
