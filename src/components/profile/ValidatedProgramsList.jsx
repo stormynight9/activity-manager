00534e7273c02af3a5eb113d1d339a6920d1ccd7
stormynight9 <ferjani.nader@hotmail.fr>
2022-06-22 05:10:28 +0100

    fixed when user has 0 validated programs

--- src/components/profile/ValidatedProgramsList.jsx
+++ src/components/profile/ValidatedProgramsList.jsx
@@ -6,6 +6,15 @@ const ValidatedProgramsList = () => {
     const userCtx = useContext(UserContext)
     console.log(userCtx.userDetails.validatedPrograms)
 
+    if (userCtx.userDetails.validatedPrograms.length === 0) {
+        return (
+            <div className="flex flex-col items-center justify-center">
+                <h2 className="text-3xl text-gray-700">Vous n'avez pas de programme validé</h2>
+            </div>
+        )
+    }
+
+
     return (
         <div className='flex flex-col space-y-2 divide-y-[1px] bg-gray-50'>
             {userCtx.userDetails.validatedPrograms.map((programId) => {
